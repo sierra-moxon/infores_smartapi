69c5ca50a2aecd8d8173b0aba1ef048f4fb0dca1
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-06-17 14:49:54 -0700

    add basics

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as FH:
+    REQUIREMENTS = FH.readlines()
+
+NAME = 'infores_smartapi'
+VERSION = '0.0.1'
+URL = 'https://github.com/sierra-moxon/infores_smartapi'
+AUTHOR = 'Sierra Moxon'
+EMAIL = 'smoxon@lbl.gov'
+REQUIRES_PYTHON = '>=3.7'
+LICENSE = 'BSD'
+
+setup(
+    name=NAME,
+    author=AUTHOR,
+    version=VERSION,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license=LICENSE,
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    install_requires=[r for r in REQUIREMENTS if not r.startswith("#")],
+    keywords='NCATS NCATS-Translator Biolink-Model',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3'
+    ],
+)
